e11c0c6a4301f64f14322432b0061c54d1965df4
Matt Brassey <matt@brassey.io>
2023-02-20 01:15:59 -0700

    fix

--- manifests/setup-cluster.sh
+++ manifests/setup-cluster.sh
@@ -42,7 +42,7 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # alias kubectl to k
